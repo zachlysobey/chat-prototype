d912896c92491bd89a16f7f36405bbd39aae35b7
Zach Lysobey <zlysobey@gmail.com>
2019-10-13 12:43:17 -0400

    (server) add basic /ws http endpoint

--- server/src/main.go
+++ server/src/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("Hello, World!")
+
+	http.HandleFunc("/ws", handleWs)
+
+	log.Println("http server started on http://localhost:8000")
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
+func handleWs(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	log.Println("handleWs", r)
+	bytes := []byte("foo bar")
+	w.Write(bytes)
+}
